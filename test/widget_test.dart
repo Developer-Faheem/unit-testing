c939755bb57a294f7d0ae99e6f53ddf9aa090f18
Developer-Faheem <dev.faheemabbas@gmail.com>
2024-07-19 23:14:46 +0500

    unit testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,12 +12,51 @@ import 'package:unittesting/counter.dart';
 import 'package:unittesting/main.dart';
 
 void main() {
-  test('check initially the coun variable is 0', () {
-//arrange
-    final Counter counterController = Counter();
-//act
-    int count = counterController.count;
-//assert
-    expect(count, 0);
+  group('testing the counter class', () {
+    late Counter counterController;
+    setUp(
+      () {
+        counterController = Counter();
+      },
+    );
+
+    test('check initially the count variable is 0', () {
+      //arrange
+      //final Counter counterController = Counter();
+      //act
+      int count = counterController.count;
+      //assert
+      expect(count, 0);
+    });
+
+    test('checking the incrementcounter function', () {
+      //arrange
+      //final Counter counterController = Counter();
+      //act
+      counterController.incrementCount();
+      int count = counterController.count;
+      //assert
+      expect(count, 1);
+    });
+
+    test('checking the decrementcounter function', () {
+      //arrange
+      //final Counter counterController = Counter();
+      //act
+      counterController.decrementCount();
+      int count = counterController.count;
+      //assert
+      expect(count, -1);
+    });
+
+    test('checking the reset function', () {
+      //arrange
+      //final Counter counterController = Counter();
+      //act
+      counterController.resetCount();
+      int count = counterController.count;
+      //assert
+      expect(count, 0);
+    });
   });
 }
